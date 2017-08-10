9ae6558c7179e1460c85dd56fac8290f41a462f9
FelipeMendes <ltdalipe@gmail.com>
2017-08-09 21:06:15 -0300

    commit dia 09/08 // criado adição de consulta de todos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var todos = require('./routes/todos');
 // inicia o express
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/todos', todos);
 // captura os erro 404
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var todos = [];
+var todoIndex = 0;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Ola Node JS! NodeMon");
+  console.log("Recebemos uma requisição GET");
+
+  res.status(200).json(todos);
+});
+router.post('/', function (req, res, next){
+  console.log("Recebemos uma requisição POST");
+
+
+
+  var todo = req.body;
+todo.id = todoIndex++;
+  todos.push(todo);
+
+  res.status(200).send();
 });
 
 module.exports = router;
