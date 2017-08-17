abb8a2602d6845c71c6275df02a9661c2ff520a7
FelipeMendes <ltdalipe@gmail.com>
2017-08-16 21:02:52 -0300

    Delete, Alter, Debug

--- routes/todos.js
+++ routes/todos.js
@@ -14,18 +14,13 @@ todos.get('/', function (req, res, next) {
 todos.get('/:todoId', function (req, res, next) {
     console.log("Recebemos uma requisição GET");
 
-    var todosFiltered =
-        todosList.filter(function (todo, index) {
-            return todo.id === parseInt(req.params.todoId);
-        });
+  var todo = findTodoById(req.params.todoId);
 
-    if (todosFiltered.length === 0) {
-        res.status(404).send();
-
+    if (todo){
+        res.status(200).json(todo);
     } else {
-        res.status(200).json(todosFiltered[0]);
+        res.status(404).send();
     }
-
 });
 
 
@@ -34,11 +29,51 @@ todos.post('/', function (req, res, next) {
 
     var todo = req.body;
     todo.id = todoIndex++;
+    todo.completed = false;
+   
     todosList.push(todo);
 
-    res.status(200).send();
+    res.status(201).send();
 });
 
+todos.delete('/:todoId', function(req, res, next){
+    var index = todosList.findIndex(function(todo, index){
+        return todo.id === parseInt(req.params.todoId);
+    });
+if(index >= 0){
+    todosList.splice(index, 1);
+    res.status(200).send();
+}else {
+    res.status(404).send();
+}
+    
+
+   
+});
+todos.put('/:todoId', function(req, res, next){
+    var todo = findTodoById(req.params.todoId);
+
+if (todo){
+    // alteração
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.completed = req.body.completed;
+
+    res.status(200).json(todo);
+}else {
+    res.status(404).send();
+}
+
+
+});
+function findTodoById(todoId){
+    var todosFiltered = todosList.filter(function (todo, index){
+        return todo.id === parseInt(todoId);
+    });
+    if (todosFiltered.length > 0)
+        return todosFiltered[0];
+    return null;
+}
 
 
 module.exports = todos;
